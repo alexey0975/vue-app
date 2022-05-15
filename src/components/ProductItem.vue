26bb1544259b9d7d49d5e754963670ce69d9e3c2
Алексей Благиных <alexey0975@yandex.ru>
2022-05-15 22:48:21 +0500

    filter and pagination added

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,43 +1,29 @@
 <template>
-<li class="catalog__item">
-      <a class="catalog__pic" href="#">
-        <img :src="product.image" :alt="product.title">
+  <li class="catalog__item">
+    <a class="catalog__pic" href="#">
+      <img :src="product.image" :alt="product.title">
+    </a>
+
+    <h3 class="catalog__title">
+      <a href="#">
+        {{ product.title }}
       </a>
+    </h3>
 
-      <h3 class="catalog__title">
-        <a href="#">
-          {{ product.title }}
-        </a>
-      </h3>
+    <span class="catalog__price">
+      {{ product.price }} ₽
+    </span>
 
-      <span class="catalog__price">
-        {{ product.price }} ₽
-      </span>
-
-      <ul class="colors colors--black">
-        <li class="colors__item">
-          <label class="colors__label">
-            <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">
-            <span class="colors__value" style="background-color: #73B6EA;">
-            </span>
-          </label>
-        </li>
-        <li class="colors__item">
-          <label class="colors__label">
-            <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
-            <span class="colors__value" style="background-color: #8BE000;">
-            </span>
-          </label>
-        </li>
-        <li class="colors__item">
-          <label class="colors__label">
-            <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
-            <span class="colors__value" style="background-color: #222;">
-            </span>
-          </label>
-        </li>
-      </ul>
-    </li>
+    <ul class="colors colors--black">
+      <li class="colors__item" v-for="color in product.colors" :key="color">
+        <label class="colors__label">
+          <input class="colors__radio sr-only" type="radio" :value="color" name="color">
+          <span class="colors__value" :style="{ backgroundColor: color, border: '1px solid #a7a7a7' }">
+          </span>
+        </label>
+      </li>
+    </ul>
+  </li>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
-import firstMessage, { secondMessage } from './data/messages';
-import showMessage from './showMessage';
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
-
-showMessage(firstMessage);
-showMessage(secondMessage);
