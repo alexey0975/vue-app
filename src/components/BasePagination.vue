a3fb8cbc550fa6cef04709fdb00a7ead773d49db
Алексей Благиных <alexey0975@yandex.ru>
2022-05-17 21:18:53 +0500

    pagitnation fixed

--- src/components/BasePagination.vue
+++ src/components/BasePagination.vue
@@ -2,7 +2,7 @@
   <ul class="catalog__pagination pagination">
     <li class="pagination__item">
       <a class="pagination__link pagination__link--arrow" :class="{ 'pagination__link--disabled': page === 1 }" href="#"
-        aria-label="Предыдущая страница" @click.prevent="paginate(page - 1)">
+        aria-label="Предыдущая страница" @click.prevent="paginatePrew(page)">
         <svg width="8" height="14" fill="currentColor">
           <use xlink:href="#icon-arrow-left"></use>
         </svg>
@@ -18,7 +18,7 @@
 
     <li class="pagination__item">
       <a class="pagination__link pagination__link--arrow" :class="{ 'pagination__link--disabled': pages === page }"
-        href="#" aria-label="Следующая страница" @click.prevent="paginate(page + 1)">
+        href="#" aria-label="Следующая страница" @click.prevent="paginateNext(page)">
         <svg width=" 8" height="14" fill="currentColor">
           <use xlink:href="#icon-arrow-right"></use>
         </svg>
@@ -40,9 +40,19 @@ export default {
     },
   },
   methods: {
-    paginate(page, pages = this.pages) {
-      if (page >= 1 && page <= pages) {
+    paginate(page) {
+      if (page >= 1 && page <= this.pages) {
         this.$emit('paginate', page);
+      }
+    },
+    paginateNext(page) {
+      if (page >= 1 && page <= this.pages) {
+        this.$emit('paginate', page + 1);
+      }
+    },
+    paginatePrew(page) {
+      if (page >= 1 && page <= this.pages) {
+        this.$emit('paginate', page - 1);
       }
     },
   },
