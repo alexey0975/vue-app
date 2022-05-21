bcad031838f2d52fa661924d37de6cd942d862a1
Алексей Благиных <alexey0975@yandex.ru>
2022-05-21 15:11:50 +0500

    wordEndFormat revised

--- src/helpers/wordEndFormat.js
+++ src/helpers/wordEndFormat.js
@@ -1,17 +1,13 @@
 export default function wordEndFormat(word, number) {
-  const numToStr = String(number);
-  const lastDigit = Number(numToStr[numToStr.length - 1]);
-  const secondDigit = Number(numToStr[numToStr.length - 2]);
-
-  if (secondDigit >= 11 && secondDigit <= 20) {
+  if (number % 100 >= 11 && number % 100 <= 20) {
     return `${word}ов`;
   }
 
-  if (lastDigit === 1) {
+  if (number % 10 === 1) {
     return word;
   }
 
-  if (lastDigit >= 2 && lastDigit <= 4) {
+  if (number % 10 >= 2 && number % 10 <= 4) {
     return `${word}a`;
   }
 
